3239024f9e73108b4192d557dd66f0f9068b868a
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2019-10-17 11:39:52 +0300

    Add files via upload

---
+++ Nancy-O-lab-1-1.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream> 
+#include <cmath> 
+#include <clocale> 
+
+using namespace std;
+
+
+int main() {
+
+	setlocale(LC_ALL, "Russian");
+
+	int a, b;
+	cout << "Введите первое ненулевое число: " << endl;
+	cin>> a;
+
+	cout << "Введите второе ненулевое число: " << endl;
+	cin >> b;
+
+	
+
+	while (a * b == 0) {
+		cout << "Одно из чисел равняется нулю, повторите попытку!" << endl;
+
+		cout << "Введите первое ненулевое число: " << endl;
+		cin >> a;
+
+		cout << "Введите второе ненулевое число: " << endl;
+		cin >> b;
+
+
+
+	}
+
+
+	double sum, res, mult;
+	double div;
+
+	sum = pow(a, 2) + pow(b, 2);
+	res = pow(a, 2) - pow(b, 2);
+	mult = pow(a, 2) * pow(b, 2);
+	div = (double)pow(a, 2) / pow(b, 2);
+
+	cout << "Сумма квадратов чисел: " << sum << endl;
+	cout << "Разница квадратов чисел: " << res << endl;
+	cout << "Произведение квадратов чисел: " << mult << endl;
+	cout << "Частное квадратов чисел: " << div << endl;
+
+	return 0;
+
+
+}
