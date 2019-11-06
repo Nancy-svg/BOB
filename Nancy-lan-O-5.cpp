b15db50db5d66722602601f683b1e19501b235fe
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2019-11-06 19:20:44 +0300

    Add files via upload

---
+++ Nancy-lan-O-5.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>   
+#include <cmath>
+using namespace std;
+int main()
+{
+	int i, j, k, n;
+	cout << "Enter N" << endl;
+	cin >> n;
+
+	int* array = new int[n];
+	cout << "Enter array" << endl;
+
+	for (i = 0;i < n;i++) {
+		cout << "array[" << i + 1 << "] = ";
+		cin >> array[i];
+
+	}
+	cout << endl;
+
+	cout << "arrai is : ";
+	for (i = 0;i < n;i++) {
+		cout << array[i] << " ";
+	}
+	cout << endl;
+	for (i = 1;i < n;i++) {
+		k = array[i];
+		j = i;
+		while (j > 0 && array[j - 1] > k) {
+			array[j] = array[j - 1];
+			j = j - 1;
+		}
+		array[j] = k;
+	}
+
+	cout << "after array is : ";
+	for (i = 0;i < n;i++) {
+		cout << array[i] << " ";
+	}
+	return 0;
+}
