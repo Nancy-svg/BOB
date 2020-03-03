b4de220cd6912c608b6f9217cb50549c3a288d41
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2020-03-03 08:03:15 +0300

    Add files via upload

---
+++ Nancy-Ex.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "ry");
+	string S, c;
+	fstream text1;
+	fstream text2;
+	text1.open("text1.txt");
+	text2.open("text2.txt");
+	getline(text2, S , '\0');
+		for (int i = 0;i < S.size(); i++) {
+		text1 << S.at(i);
+	}
+	text1.close();
+	text2.close();
+return 0;
+}	
---
+++ Nancy-Ex1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+void quicksort(int* arr, int first, int last)
+{
+	if (first < last)
+	{
+		int buf, left = first, right = last, pivot = arr[(left + right) / 2];
+		do
+		{
+			while (arr[left] < pivot) left++;
+			while (arr[right] > pivot) right--;
+			if (left <= right)
+			{
+				buf = arr[left];
+				arr[left] = arr[right];
+				arr[right] = buf;
+				left++;
+				right--;
+			}
+		} while (left <= right);
+		quicksort(arr, first, right);
+		quicksort(arr, left, last);
+	}
+}
+
+int main() {
+	int arr[] = { 1, 4, 8, 0, 2, 7 };
+	int N = sizeof(arr) / sizeof(int);
+	quicksort(arr, 0, N - 1);
+	for (int i = 0; i < 6; i++) {
+		cout << arr[i] << " ";
+	}
+	return 0;
+}
+
