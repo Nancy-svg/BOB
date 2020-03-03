db3049a009db39ea026140f844e2ee9d22d7636f
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2020-03-03 07:39:05 +0300

    Add files via upload

---
+++ numerical3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+double func(double x) {
+    return 2 * x - log10(x) - 7;
+}
+int main()
+{
+    double xn = 3, x,b = 4,p = 0.0001, fa, fb;
+    do {
+        x = xn;
+        xn = x - (b - x) / (func(b) - func(x)) * func(x);
+    } while (abs(func(xn) - func(x)) > p);
+    cout << x;
+}
+
