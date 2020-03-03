83c6d28d65b404c542484c55e7ad1298bbe584b7
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2020-03-03 08:04:31 +0300

    Add files via upload

---
+++ myd.2day.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+ float q(float a) {
+	 float q;
+	 q = 2 * a - log10(a) - 7;
+	 return(q);
+}
+int main()
+{
+	float a, b, fa, fb , k,k3;
+	int k1, k2;
+	fb = 0;
+	a = 3;
+	b = 4;
+	k = q(a);
+	do 
+	{
+		k3 = a;
+		a = (a + b) / 2;
+		fa = q(a);
+		if (k * fa < 0) {
+			b = k3;
+		}
+		k1 = q(a) * 100;
+		k2 = q(b) * 100;
+		k = fa;
+		cout << a << endl;
+	} 
+	while (k1 != k2);
+	return 0;
+}
---
+++ myd.day3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	float fn, result;
+	int k;
+	fn = 4;
+	do {
+		result = fn - (2 * fn - log10(fn) - 7) / (2 - 1 / fn / log(10));
+		k = (result - fn) * 10000;
+		fn = result;
+	} while (k > 0);
+	cout << result;
+	return 0;
+}
