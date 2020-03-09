ac627d71a00afd6e49af38fa40e11840fa0c3673
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2020-03-09 21:42:20 +0300

    Update myd.day3.cpp

--- myd.day3.cpp
+++ myd.day3.cpp
@@ -1,16 +1,17 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
 using namespace std;
 int main()
 {
 	float fn, result;
-	int k;
-	fn = 4;
+	int k , i = 0;;
+	result = 4;
 	do {
+	    fn = result;
 		result = fn - (2 * fn - log10(fn) - 7) / (2 - 1 / fn / log(10));
-		k = (result - fn) * 10000;
-		fn = result;
-	} while (k > 0);
-	cout << result;
+		i++;
+	} while (abs(result - fn) > 0.00001);
+	cout << "Результат равен " << result << endl;
+	cout << "Количество итераций " << i;
 	return 0;
 }
