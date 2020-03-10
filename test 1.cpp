94bcb1d518b87dc2fad365267b185c9c8f308b24
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2020-03-10 07:08:19 +0300

    Add files via upload

---
+++ test 1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+ float q(float a) {
+	 float q;
+	 q = 2 * a - log10(a) - 7;
+	 return(q);
+}
+int main() {
+	double fx = 3, fn = 4, k;
+int i = 0;
+do
+{
+	fn = q(fx);
+	k = abs(fx - fn);
+	fx = fn;
+	i++;
+}
+while(k > 0.001);
+cout << "Результат равен " << fx << endl;
+cout << "Количество итераций " << i;
+}
